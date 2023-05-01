5a1ca844c379d40096c87a4b75c18f9881bec55b
Denis <oumniy@gmail.com>
2023-05-01 20:34:34 +0300

    задача с выводом третьтей цифры

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+if (number/100 != 0) {
+    int number2 = number/100;
+    System.Console.WriteLine($"третья цифра числа {number} - {number2%10}");}
+else {
+    System.Console.WriteLine("третьей цифры нет");
+}
